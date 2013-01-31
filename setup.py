9379cbfc60a01422425e788a92a38b7839357a60
Rob Martin <rob@version2beta.com>
2013-01-31 08:57:56 -0600

    Packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='Syslogger',
+    version='0.0.1',
+    author='Rob Martin @version2beta',
+    author_email='rob@version2beta.com',
+    packages=['syslogger'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/syslogger/',
+    license='LICENSE.txt',
+    description='Log messages by facility and level.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    package_data={
+        '': ['*.dist'],
+      },
+    entry_points={
+        'console_scripts': [
+          'syslogger = syslogger:main',
+        ],
+      },
+)
